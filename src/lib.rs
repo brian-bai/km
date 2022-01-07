af2451122a17f1dde33fe8e4387433ff6eb16732
brian bai <baitq@outlook.com>
2022-01-07 21:59:00 +0800

    add rusqlite

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
 use std::path::Path;
+use rusqlite::{Connection, Result};
 
 #[macro_export]
 macro_rules! crate_version {
     () => {
         env!("CARGO_PKG_VERSION")
     };
+}
+
+fn get_conn() -> Result<Connection> {
+    let path = &shellexpand::tilde("~/.dbdir/bookstore.db").into_owned();
+    let path = Path::new(path);
+    Connection::open(path)
+}
+pub fn read_tags() -> Result<Vec<String>> {
+    let conn = get_conn()?;
+    let mut stmt = conn.prepare("SELECT tag from book_marks ;")?;
+    let rows = stmt.query_map([], |row| Ok(row.get(0)))?;
+    let mut tags = Vec::new();
+    for row in rows {
+        tags.push(row.unwrap()?);
+    }
+    Ok(tags)
 }
